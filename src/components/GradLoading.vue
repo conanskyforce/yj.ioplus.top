2ec9d6d6e74d63ca01be808cffa8e7efa2619ecc
conanskyforce <conanskyforce@163.com>
2020-07-18 17:22:36 +0800

    feat: add new component

---
+++ src/components/GradLoading.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loading">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.loading {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  $colors: #7ef9ff, #89cff0, #4682b4, #0f52ba, #000080;
+  display: flex;
+  justify-content: center;
+  animation-delay: 1s;
+
+  .dot {
+    position: relative;
+    width: 3em;
+    height: 3em;
+    margin: 0.8em;
+    border-radius: 50%;
+
+    &::before {
+      position: absolute;
+      left: 0;
+      content: "";
+      width: 100%;
+      height: 100%;
+      background: inherit;
+      border-radius: inherit;
+      animation: wave 2s ease-out infinite;
+    }
+
+    @for $i from 1 through 5 {
+      &:nth-child(#{$i}) {
+        background: nth($colors, $i);
+
+        &::before {
+          animation-delay: $i * 0.2s;
+        }
+      }
+    }
+  }
+}
+
+@keyframes wave {
+  50%,
+  75% {
+    transform: scale(2.5);
+  }
+
+  80%,
+  100% {
+    opacity: 0;
+  }
+}
+</style>
