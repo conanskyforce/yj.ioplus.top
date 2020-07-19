b79cfb41a8c4c57de579f757eb3e03bad0caa335
conanskyforce <conanskyforce@163.com>
2020-07-19 18:16:21 +0800

    feat: 七牛cdn

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
         limit: 10, // 以下配置项用于配置file-loader
         // 根据环境使用cdn或相对路径
         // publicPath: process.env.NODE_ENV === 'production' ? 'http://static.ioplus.top/conan-static/static.ioplus.top/' : './',
-        // publicPath: 'http://static.ioplus.top/conan-static/static.ioplus.top/',
+        publicPath: 'http://qiqiu.ioplus.top/',
         // 将图片打包到dist/img文件夹下, 不配置则打包到dist文件夹下
         outputPath: 'assets',
         // 配置打包后图片文件名
