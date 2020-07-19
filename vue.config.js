37ad03c290b535338256015aff85053f3bfcc670
conanskyforce <conanskyforce@163.com>
2020-07-19 18:20:11 +0800

    feat: url 路径

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
         // publicPath: process.env.NODE_ENV === 'production' ? 'http://static.ioplus.top/conan-static/static.ioplus.top/' : './',
         publicPath: 'http://qiqiu.ioplus.top/',
         // 将图片打包到dist/img文件夹下, 不配置则打包到dist文件夹下
-        outputPath: 'assets',
+        // outputPath: 'assets',
         // 配置打包后图片文件名
         name: 'images/[name]-[hash].[ext]',
       })
