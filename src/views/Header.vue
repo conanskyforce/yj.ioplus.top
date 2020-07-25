c71cd7738e3f019a93bba6e768ecd2ee42970867
conanskyforce <conanskyforce@163.com>
2020-07-25 22:33:45 +0800

    feat: update style

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="header">
+  <div :class="{header: true,'header-fixed':headerFixed}">
     <Navigation />
   </div>
 </template>
 <script>
 export default {
-  name: 'my-header'
+  name: 'my-header',
+  data(){
+    return {
+      headerFixed: false
+    }
+  },
+  created () {
+    window.onscroll = this.throttle(this.setHeader)
+  },
+  destroyed() {
+    window.onscroll = null
+  },
+  methods: {
+    setHeader () {
+      if(document.documentElement.scrollTop > 0) {
+        this.headerFixed = true
+      } else {
+        this.headerFixed = false
+      }
+    },
+    throttle (fn, delay = 100) {
+      let timer
+      return function(){
+        if(timer) return
+        timer = setTimeout(()=>{
+          fn()
+          timer = null
+        },delay)
+      }
+    }
+  },
 }
 </script>
 <style lang="less" scoped>
@@ -22,9 +52,14 @@ export default {
   z-index: 2;
   transition: all 0.5s;
 }
+.header-fixed{
+  position: fixed;
+  background: rgba(255,255,255,0.8);
+  box-shadow: 0px 2px 8px lightgrey;
+}
 @media screen and (min-width: 600px){
-  .header:hover{
-    background: rgba(0,0,0,0.3);
-  }
+  // .header:hover{
+  //   background: rgba(255,255,255,0.8);
+  // }
 }
 </style>
