3d19c8dd43f5f2fe77b11b05e619c1f15af7e646
conanskyforce <conanskyforce@163.com>
2020-07-26 16:35:04 +0800

    feat: change stylde

---
+++ src/views/BigHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="big-home">
+    <div class="index-bg">
+
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.big-home {
+  background-image: url(https://s3.pstatp.com/ee/feishu_website/static/img/header-bg.4db2d98100.svg);
+  background-size: cover;
+  .index-bg{
+    background-image: url(https://s1.pstatp.com/ee/feishu_website/static/img/person.72b309fb65.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    max-width: 1200px;
+    height: 580px;
+    margin: 0 auto;
+  }
+}
+</style>
